0e5eedcbb3535aac40c8181f28a888cf72b3e8bb
wis003 <wis003@ucsd.edu>
2021-03-30 20:16:48 -0700

    Problem 148

--- 148/ListNode.java
+++ 148/ListNode.java
@@ -7,27 +7,52 @@ public class ListNode {
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
-    ListNode sortList(ListNode head) {
-        ListNode tracker = head;
-        if(head == null) {
-            return head;
+    public ListNode merge(ListNode h1, ListNode h2){
+        if(h1 == null){
+            return h2;
         }
-        while(tracker.next != null) {
-            ListNode curr = head;
-            while(curr.next != null) {
-                if(curr.val > curr.next.val) {
-                    swapValue(curr, curr.next);
-                }
-                curr = curr.next;
-            }
-            tracker = tracker.next;
+        if(h2 == null){
+            return h1;
         }
-        return head;
+        
+        if(h1.val < h2.val){
+            h1.next = merge(h1.next, h2);
+            return h1;
+        }
+        else{
+            h2.next = merge(h1, h2.next);
+            return h2;
+        }
+        
     }
     
-    void swapValue(ListNode n1, ListNode n2) {
-        int temp = n1.val;
-        n1.val = n2.val;
-        n2.val = temp;
+    public ListNode sortList(ListNode head) {
+        //bottom case
+        if(head == null){
+            return head;
+        }
+        if(head.next == null){
+            return head;
+        }
+        
+        //p1 move 1 step every time, p2 move 2 step every time, pre record node before p1
+        ListNode p1 = head;
+        ListNode p2 = head;
+        ListNode pre = head;
+        
+        while(p2 != null && p2.next != null){
+            pre = p1;
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        //change pre next to null, make two sub list(head to pre, p1 to p2)
+        pre.next = null;
+        
+        //handle those two sub list
+        ListNode h1 = sortList(head);
+        ListNode h2 = sortList(p1);
+        
+        return merge(h1, h2);
+        
     }
 }
