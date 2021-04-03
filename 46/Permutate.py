322ebaf9f9f873a5680261d856a1282e6dbbf7cd
wis003 <wis003@ucsd.edu>
2021-04-02 19:59:17 -0700

    Problem 46

--- 46/Permutate.py
+++ 46/Permutate.py
@@ -2,14 +2,15 @@
 
 def permute(nums):
     out = []
-    if len(nums) == 0:
-        return out
+    if len(nums) == 1:
+        return [nums]
     
     else:
         for i in range(len(nums)):
             temp = nums[:]
             temp.remove(nums[i])
-            out.append([nums[i]] + permute(temp))
+            for permutations in permute(temp):
+                out.append([nums[i]] + permutations)
         return out
 
 test1 = [1, 2, 3]
