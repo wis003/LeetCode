7cfe9045f4797485ab861e005c102ed4a245748c
wis003 <wis003@ucsd.edu>
2021-04-03 20:15:48 -0700

    Problem 215

--- 215/findKthLargest.java
+++ 215/findKthLargest.java
@@ -8,6 +8,8 @@ class Solution {
 
     public static void main(String ars[]) {
         int[] test1 = {3, 2, 1, 5, 6, 4};
+        int[] test2 = {3, 2, 3, 1, 2, 4, 5, 5, 6};
         System.out.println(findKthLargest(test1, 2));
+        System.out.println(findKthLargest(test2, 4));
     }
 }
