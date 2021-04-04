48e0b90d0413f68ef6d3c99300a92fcf2cc27c7d
wis003 <wis003@ucsd.edu>
2021-04-03 20:14:29 -0700

    Problem 215

--- 215/findKthLargest.java
+++ 215/findKthLargest.java
@@ -1,8 +1,13 @@
 import java.util.Arrays;
 
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
+    public static int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
+    }
+
+    public static void main(String ars[]) {
+        int[] test1 = {3, 2, 1, 5, 6, 4};
+        System.out.println(findKthLargest(test1, 2));
     }
 }
