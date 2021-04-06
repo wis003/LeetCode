543f353607e0e446eb9461d14c297abaac049296
wis003 <wis003@ucsd.edu>
2021-04-05 20:26:27 -0700

    Problem 48

--- 48/rotate.cpp
+++ 48/rotate.cpp
@@ -1,8 +1,36 @@
-# Rotate Image
+// Rotate Image
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 class Solution {
     public:
         void rotate(vector<vector<int>>& matrix) {
-            
+            int length = matrix.size();
+            for(int row = 0; row < length; row++) {
+                for(int column = 0; column < length; column++) {
+                    if(column >= row && column < length - row - 1) { // positions to operate on
+                        int i = 0;
+                        int nextRow = row;
+                        int nextColumn = column;
+                        int currValue = matrix[nextRow][nextColumn];
+                        while(i < 4) {
+                            
+                            // [x, y] --> [y, length - x - 1]
+                            int temp = nextRow;
+                            nextRow = nextColumn;
+                            nextColumn = length - temp - 1;
+
+                            // swap
+                            int nextValue = matrix[nextRow][nextColumn];
+                            matrix[nextRow][nextColumn] = currValue;
+                            currValue = nextValue;
+                            i++;
+                        }
+                    }
+                }
+            }
         }
 };
