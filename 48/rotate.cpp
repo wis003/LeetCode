0cf5f83f206226105e8d4f9bb495a88a4e5911cd
wis003 <wis003@ucsd.edu>
2021-04-05 20:29:40 -0700

    Problem 48

--- 48/rotate.cpp
+++ 48/rotate.cpp
@@ -12,11 +12,13 @@ class Solution {
             for(int row = 0; row < length; row++) {
                 for(int column = 0; column < length; column++) {
                     if(column >= row && column < length - row - 1) { // positions to operate on
+
                         int i = 0;
                         int nextRow = row;
                         int nextColumn = column;
                         int currValue = matrix[nextRow][nextColumn];
-                        while(i < 4) {
+
+                        while(i < 4) { // each pairing has 4 rotations (except for middle element)
                             
                             // [x, y] --> [y, length - x - 1]
                             int temp = nextRow;
@@ -37,12 +39,14 @@ class Solution {
 
 int main() {
     Solution object;
-    vector<vector<int>> matrix {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
-    object.rotate(matrix);
-    for(auto i : matrix) {
+    vector<vector<int>> test2 {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+    object.rotate(test2);
+    for(auto i : test2) {
         for(auto j : i) {
             cout << j << " ";
         }
     }
+    cout << endl;
+
     return 0;
 }
