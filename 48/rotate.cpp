b5ed697967a462cb4ebf4b48e7edc65430238f06
wis003 <wis003@ucsd.edu>
2021-04-05 20:31:25 -0700

    Problem 48

--- 48/rotate.cpp
+++ 48/rotate.cpp
@@ -39,13 +39,24 @@ class Solution {
 
 int main() {
     Solution object;
+    vector<vector<int>> test1 {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     vector<vector<int>> test2 {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+    object.rotate(test1);
     object.rotate(test2);
-    for(auto i : test2) {
+
+    for(auto i : test1) {
         for(auto j : i) {
             cout << j << " ";
         }
     }
 
+    cout << endl;
+
+    for(auto i : test2) {
+        for(auto j : i) {
+            cout << j << " ";
+        }
+    }
+    
     return 0;
 }
