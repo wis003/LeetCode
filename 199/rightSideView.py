15044635c7380d9bd3339ed7498c3bd0c0b0427b
wis003 <wis003@ucsd.edu>
2021-04-07 19:18:30 -0700

    Problem 199

---
+++ 199/rightSideView.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        out = []
+        while root:
+            out.append(root.val)
+            if root.right:
+                root = root.right
+            else:
+                root = root.left
+        return out     
