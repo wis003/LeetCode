cf34accbb88350fc2adcfac9b98ba1d831aae93d
wis003 <wis003@ucsd.edu>
2021-04-08 19:55:56 -0700

    Problem 199

--- 199/rightSideView.py
+++ 199/rightSideView.py
@@ -16,18 +16,26 @@ class Solution(object):
         out = []
         if root is not None:
             queue.append(root)
-
             
-            while len(queue) > 0:
-                currLevel = []
-                for i in range(len(queue)):
-                    currTreeNode = queue.pop(0)
-                    currLevel.append(currTreeNode.val)
-                    if currTreeNode.left is not None:
-                        queue.append(currTreeNode.left)
-                    if currTreeNode.right is not None:
-                        queue.append(currTreeNode.right)
-                levelOrderView.append(currLevel)
+        while len(queue) > 0:
+            currLevel = []
+            for i in range(len(queue)):
+                currTreeNode = queue.pop(0)
+                currLevel.append(currTreeNode.val)
+                if currTreeNode.left is not None:
+                    queue.append(currTreeNode.left)
+                if currTreeNode.right is not None:
+                    queue.append(currTreeNode.right)
+            levelOrderView.append(currLevel)
         for level in levelOrderView:
             out.append(level[len(level) - 1])
         return out
+
+test1 = TreeNode(1)
+test1.left = TreeNode(2)
+test1.right = TreeNode(3)
+test1.left.right = TreeNode(5)
+test1.right.right = TreeNode(4)
+
+solutionTest = Solution()
+print(str(solutionTest.rightSideView(test1)))
