3a126352ffd7a990de82b99a4271192ea127d028
wis003 <wis003@ucsd.edu>
2021-04-09 20:44:56 -0700

    Problem 236

--- 236/lowestCommonAncestor.cpp
+++ 236/lowestCommonAncestor.cpp
@@ -1,12 +1,10 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
